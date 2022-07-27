a63305ecdef64fc6a51f1fdf0db6635f3e979d6f
manpreetdi99 <61454143+manpreetdi99@users.noreply.github.com>
2022-07-28 00:47:10 +0300

    safe_example

--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,19 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {}
+class MyApp extends StatelessWidget {
+  @override //μια φορα δεσμευση μνημης
+  num days = 30;
+  String name = "manpreet";
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("welcome to $days days flutter $name"),
+          ),
+        ),
+      ),
+    );
+  }
+}
