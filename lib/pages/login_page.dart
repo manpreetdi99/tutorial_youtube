71f96ba66a79afcc45fb5dcbc6147c603cce447b
manpreetdi99 <61454143+manpreetdi99@users.noreply.github.com>
2022-08-01 01:35:46 +0300

    day6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:youtube_tut/utils/routes.dart';
 import 'home_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   //state=Κατασταση Stateless οχι δυναμικο, Statefullδυναμικο
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     //Προσοχη με το row και colomns
@@ -17,11 +25,11 @@ class LoginPage extends StatelessWidget {
             Image.asset("assets/images/20943504.jpg", fit: BoxFit.cover),
             //SizedBox(height: 20),//κενο ενδιαμεσα φωτογραφιας και κειμενο (column)
             Text(
-              "Wellcome",
+              "Wellcome $name",
               style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
-                  color: Colors.deepPurpleAccent),
+                  color: Colors.deepPurple),
             ),
             SizedBox(height: 20),
             Padding(
@@ -34,6 +42,11 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter username",
                       labelText: "Username",
                     ),
+                    onChanged: (value) {
+                      //για να εμφανιζει το ονομα μαζι με το wellcome
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -45,15 +58,48 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 30,
                   ),
-                  ElevatedButton(
-                    //δημιουργεια κουμπιου
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    onPressed: () {
-                      Navigator.pushNamed(
-                          context, MyRoutes.homeRoute); //να αλλαξει οθονη
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                  )
+                    //alignment: Alignment.center, //ευθιγραμμιση κεντρου
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8),
+                      ), //συμηλευση γωνιων
+                    ),
+                  ),
+                  // ElevatedButton(
+                  //   //δημιουργεια κουμπιου
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(
+                  //         context, MyRoutes.homeRoute); //να αλλαξει οθονη
+                  //   },
+                  // )
                 ],
               ),
             ),
